de9cec14b13351be7c84e1d86c11e3b64f786e24
chandu <chandubanoth980@gmail.com>
2021-10-19 09:44:18 +0530

    first commit

---
+++ springrest/src/main/java/com/springrest/springrest/controller/MyController.java
@@ -0,0 +1,60 @@
+package com.springrest.springrest.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.springrest.springrest.entities.Course;
+import com.springrest.springrest.services.CourseService;
+
+import java.util.*;
+
+@RestController
+public class MyController {
+	@Autowired
+	private CourseService courseService;
+	
+      @GetMapping("/home")
+	  public String home() {
+		  return "Welcome to courses application";
+	  }
+      
+      
+      //get the courses 
+      
+      @GetMapping("/courses")
+      public List<Course> getCourses()
+      {
+    	  return this.courseService.getCourses();
+      }
+      
+      @GetMapping("/courses/{courseId}")
+      public Course getCourse(@PathVariable String courseId)
+      {
+    	  return this.courseService.getCourse(Long.parseLong(courseId));
+      }
+      
+      @PostMapping("/courses")
+      public Course addCourse(@RequestBody Course course)
+      {
+    	  return this.courseService.addCourse(course);
+      }
+      
+      @PutMapping("/courses")
+      public Course updateCourse(@RequestBody Course course)
+      {
+    	  return this.courseService.updateCourse(course);
+      }
+      
+   
+      
+      
+}
